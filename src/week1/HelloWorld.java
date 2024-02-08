3007893649caae503785b6cb1f01e40664a6fa5e
Chuck Kiefriter <ckiefriter@gmail.com>
2024-02-07 23:18:14 -0500

    1st commit

---
+++ src/week1/HelloWorld.java
@@ -0,0 +1,10 @@
+package week1;
+public class HelloWorld {
+	// Required to make this class an executable program.
+	public static void main(String[] args) {
+			
+		// Code that executes when this class is run as a program.
+		System.out.println("Hello World");
+	}
+
+}
